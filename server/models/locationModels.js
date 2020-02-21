087b71185bc08caa800dcc09d362b032c68a8239
tinchonapa <tinchonapa@gmail.com>
2020-02-21 15:38:32 -0700

    removed mysql code

--- server/models/locationModels.js
+++ server/models/locationModels.js
@@ -1,56 +1,5 @@
 const knex = require('../db/postgres/connection/connection');
 
-// const connection = require('../db/mysql/connections/costCalculatorConnections.js');
-
-/* const postZipcodes = (data, callback) => {
-    let queryCommand = `INSERT INTO location (zipcode, taxes, fees, rate) VALUES (${data.zipcode}, ${data.taxes}, ${data.fees}, ${data.rates})`;
-    connection.query(queryCommand,(error, result, field) => {
-        if (error) { 
-            console.log(error)
-            callback(error)
-        } else {
-            callback(null, result)
-        }
-    })
-}
-
-const getZipcodes = (zipcode, callback) => {
-    const queryText = `SELECT taxes, fees, rate FROM location WHERE zipcode = ${zipcode}`;
-    connection.query(queryText, (error, result, field)=>{
-        if (error) {
-            console.log(error)
-            callback(error);
-        } else {
-            callback(null, result)
-        }
-    })
-}
-
-const updateZipcodes = (zipcode, data, callback) => {
-    let queryCommand = `UPDATE location SET rate = ${data} WHERE zipcode = ${zipcode}`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error);
-        } else {
-            callback(null, result)
-        }
-    })
-
-}
-
-const deleteZipcodes = (zipcode, callback) => {
-    let queryCommand = `DELETE FROM location WHERE zipcode = ${zipcode}`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error)
-        } else {
-            callback(null, result)
-        }
-    })
-}
-*/
 
 module.exports = {
     postZipcodes(data) {
