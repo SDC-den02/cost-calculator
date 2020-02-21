a320e43a32cee1f8a153afb43f0fdc9de5fd32c7
tinchonapa <tinchonapa@gmail.com>
2020-02-21 11:44:09 -0700

    Completed CRUD for cars

--- server/controllers/carsControllers.js
+++ server/controllers/carsControllers.js
@@ -2,7 +2,7 @@ const model = require('../models/carsModels.js');
 
 const postCars = (req, res, next) => {
   console.log('This is post ', req.body.price);
-  model.postCarPrice(req.body.price )
+  model.postCarPrice(req.body.price)
     .then( data => { 
       console.log(`Cost has been added`)
       console.log(data)
@@ -18,7 +18,7 @@ const getCars = (req, res, next) => {
   console.log('params for car', req.params)
   model.readCarPrice(req.params.id)
     .then(car => {
-      res.status(200).json({ car:result })
+      res.status(200).json(car)
     })
     .catch(error => {
       console.log(error)
@@ -27,27 +27,28 @@ const getCars = (req, res, next) => {
 }
 
 const updateCars = (req, res, next) => {
-  console.log('This is update ', req.body.price);
-  model.updateCarPrice(req.params.id, req.body.price, (error, result) => {
-    if (error) {
-      console.log(error)
-      res.status(400).send(error)
-    } else {
-      res.status(200).json({})
-    }
+  console.log(`Updating for ${req.params.id} the new price is `, req.body.price);
+  model.updateCarPrice(req.params.id, req.body.price)
+  .then( data => {
+    console.log('Total # of items updated ', data)
+    res.status(200).json({data})
+  })
+  .catch(error => {
+    console.log(error);
+    res.status(400).send(error);
   })
 }
 
 const deleteCars = (req, res, next) => {
   console.log('Deleting cost for car ID#', req.params.id);
-  model.deleteCarPrice(req.params.id, (error, result) => {
-    if (error) {
+  model.deleteCarPrice(req.params.id) 
+    .then(() => {
+      res.status(200).json({})
+    })
+    .catch(error => {
       console.log(error);
       res.status(400).send(error)
-    } else {
-      res.status(200).json({})
-    }
-  })
+    });      
 }
 
 
--- server/models/carsModels.js
+++ server/models/carsModels.js
@@ -1,61 +1,5 @@
 const knex = require('../db/postgres/connection/connection');
 
-/*
-const connection = require('../db/mysql/connections/costCalculatorConnections.js');
-
-const postCarPrice = (data, callback) => {
-    // console.log('this is data in models', data)
-    let queryCommand = `INSERT INTO cars (cost) VALUES (${data})`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error)
-        } else { 
-            callback(null, result)
-        }
-    })
-}
-
-const readCarPrice = (id, callback) => {
-    const queryText = `SELECT cars FROM cost WHERE id = ${id}`;
-    connection.query(queryText, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error)
-        } else {
-            //error first... dont forget.
-            callback(null, result)
-        }
-    })
-}
-
-const updateCarPrice = (id, data, callback) => {
-    console.log('In models data is ', data)
-    let queryCommand = `UPDATE cars SET cost = ${data} WHERE id = ${id}`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error)
-        } else {
-            callback(null, result);
-        }
-    })
-}
-
-const deleteCarPrice = (id, callback) => {
-    let queryCommand = `DELETE FROM cars WHERE id = ${id}`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error)
-        } else {
-            callback(null, result);
-        }
-    })
-}
-
-
-*/
 module.exports = {
     postCarPrice(data) {
         return knex('cars')
@@ -66,6 +10,14 @@ module.exports = {
             .where('id', id)
             .first();
     },
-    // updateCarPrice,
-    // deleteCarPrice
+    updateCarPrice(id, data) {
+        return knex('cars')
+            .where('id', id)
+            .update({price: data});
+    },
+    deleteCarPrice(id) {
+        return knex('cars')
+            .where('id', id)
+            .del();
+    }
 }
