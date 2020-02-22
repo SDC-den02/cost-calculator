d7829bffa5ac081a4631582486fff49af5d4a16c
tinchonapa <tinchonapa@gmail.com>
2020-02-21 18:28:17 -0700

    Added seeding of 10M with knex & postgres

--- seeds/02_cars.js
+++ seeds/02_cars.js
@@ -1,14 +1,17 @@
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex.raw('DELETE FROM "cars"; ALTER SEQUENCE cars_id_seq RESTART WITH 6;')
-    .then(function () {
-      // Inserts seed entries
-      return knex('cars').insert([
-        {id: 1, price: '10000'},
-        {id: 2, price: '11485'},
-        {id: 3, price: '5000'},
-        {id: 4, price: '6892'},
-        {id: 5, price: '20000'}
-      ]);
-    });
+const faker = require('faker');
+
+const createFakePrices = () => ({
+  price: faker.random.number()
+});
+
+exports.seed = async (knex) => {
+  let fakePrices = [];
+  const desiredFakePrices = 10001000;
+    for(let i = 0; i < desiredFakePrices; i++){
+      fakePrices.push(createFakePrices());
+      if (i % 1000 === 0) {
+        await knex('cars').insert(fakePrices);
+        fakePrices = [];
+      }
+    }
 };
