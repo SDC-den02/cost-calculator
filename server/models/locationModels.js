8e8f3dedd457f73ed9e7427da22deeb5d5445f83
Martin Naparstek <tinchonapa@gmail.com>
2020-02-26 16:27:54 -0700

    Cleaned code

--- server/models/locationModels.js
+++ server/models/locationModels.js
@@ -2,57 +2,6 @@ const connection = require('../connections/costCalculatorConnections.js');
 const mongoose = require('mongoose');
 const Location = require('../db/mongo/Location');
 
-/*
-const postZipcodes = (data, callback) => {
-    let queryCommand = `INSERT INTO location (zipcode, taxes, fees, rate) VALUES (${data.zipcode}, ${data.taxes}, ${data.fees}, ${data.rates})`;
-    connection.query(queryCommand,(error, result, field) => {
-        if (error) { 
-            console.log(error)
-            callback(error)
-        } else {
-            callback(null, result)
-        }
-    })
-}
-
-const getZipcodes = (zipcode, callback) => {
-    const queryText = `SELECT taxes, fees, rate FROM location WHERE zipcode = ${zipcode}`;
-    connection.query(queryText, (error, result, field)=>{
-        if (error) {
-            console.log(error)
-            callback(error);
-        } else {
-            callback(null, result)
-        }
-    })
-}
-
-const updateZipcodes = (zipcode, data, callback) => {
-    let queryCommand = `UPDATE location SET rate = ${data} WHERE zipcode = ${zipcode}`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error);
-        } else {
-            callback(null, result)
-        }
-    })
-
-}
-
-const deleteZipcodes = (zipcode, callback) => {
-    let queryCommand = `DELETE FROM location WHERE zipcode = ${zipcode}`;
-    connection.query(queryCommand, (error, result, field) => {
-        if (error) {
-            console.log(error)
-            callback(error)
-        } else {
-            callback(null, result)
-        }
-    })
-}
-*/
-
 module.exports = {
     postZipcodes(data) {
         console.log('This is data ', data);
